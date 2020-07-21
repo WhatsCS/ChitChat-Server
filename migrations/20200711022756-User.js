9a2e3173936fdc5bdc414ba8c854ac2e64754ca0
WhatsCS <whatscs@jointheb.org>
2020-07-21 15:45:11 -0700

    model migration and dev user seed

---
+++ migrations/20200711022756-User.js
@@ -0,0 +1,46 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      username: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      uid: {
+        type: Sequelize.INTEGER,
+        unique: true
+      },
+      socketID: {
+        type: Sequelize.STRING,
+        defaultValue: null
+      },
+      status: {
+        type: Sequelize.ENUM({
+          values: ['offline', 'online', 'away']
+        }),
+        defaultValue: 'offline'
+      },
+      createdAt: {
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        type: Sequelize.DATE
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users')
+  }
+}
---
+++ seeders/20200711020549-dev_user.js
@@ -0,0 +1,15 @@
+const { User } = require('../models')
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await User.create({
+      username: 'dev_user',
+      email: 'dev@jointheb.org',
+      password: 'test password',
+      uid: 1234
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+}
