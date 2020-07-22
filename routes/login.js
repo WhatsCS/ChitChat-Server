ff2fb11f94ccc92ffda299a57dd19eab194663b8
WhatsCS <whatscs@jointheb.org>
2020-07-22 13:15:41 -0700

    refactor login route

--- routes/login.js
+++ routes/login.js
@@ -4,54 +4,48 @@
  */
 const express = require('express')
 const router = express.Router()
-const dbUtils = require('../utils/database')
+const dbUtils = require('../utils/db')
 const auth = require('../utils/auth')
-const bcrypt = require('bcrypt')
 
 /**
+ * @async
  * @name login root POST
  * @desc On a POST request, it will "log" the user in and return their auth token
  * @param {string} email - users email
  * @param {string} password - users password
- * @return {express.Response} response - returns their auth token, uuid and username
+ * @return {express.Response} response - returns their auth token, uid and username
  */
-router.post('/', function (req, res) {
+router.post('/', async (req, res) => {
   const user = { email: req.body.email, password: req.body.password }
   if (user.email && user.password) {
-    dbUtils.checkUserPassword(user).then((_res) => {
-      if (_res === -1) {
-        return res.status(403).json({
-          message: 'Email or Password is incorrect'
+    const _res = await dbUtils.checkUserPassword(user)
+    if (_res) {
+      try {
+        const info = await dbUtils.userInfoEmail(user.email)
+        const token = auth.createToken(user)
+        return res.status(200).json({
+          token: token,
+          uid: info.uid,
+          username: info.username
         })
+      } catch (err) {
+        console.error(err)
+        return res.status(500)
       }
-      if (_res) {
-        let info = []
-        dbUtils.userInfoEmail(user.email).then((_res) => {
-          const token = auth.createToken(user)
-          info = _res
-          return res.status(200).json({
-            token: token,
-            uuid: info[0].uuid,
-            username: info[0].username
-          })
-        })
-          .catch(err => {
-            console.error(err)
-          })
-      } else {
-        return res.status(403).json({
-          message: 'Email or Password is incorrect'
-        })
-      }
-    })
+    } else {
+      return res.status(400).json({
+        message: 'Email or Password is incorrect'
+      })
+    }
   } else {
     return res.status(400).json({
-      message: 'Failed!'
+      message: 'Missing required fields!'
     })
   }
 })
 
 /**
+ * @async
  * @name Register Route
  * @desc Registers the user with the database.
  * @param {string} email
@@ -59,46 +53,37 @@ router.post('/', function (req, res) {
  * @param {string} password
  * @return {express.Response} response - Tells the client if they are registered or not
  */
-router.post('/register', function (req, res) {
+router.post('/register', async (req, res) => {
   const body = req.body
   if (!body.hasOwnProperty('username') || !body.hasOwnProperty('email') || !body.hasOwnProperty('password')) { // eslint-disable-line
     return res.status(400).json({
-      message: 'Something missing, please double check.'
+      message: 'Missing a field, please double check.'
     })
   }
-
-  let uuid
-  const min = Math.ceil(1000)
-  const max = Math.floor(9999)
-  uuid = Math.floor(Math.random() * (max - min + 1)) + min
-  const exists = dbUtils.usernameExists(body.username)
-  exists.then((_res) => {
-    if (_res.length > 0) {
-      dbUtils.userInfo(body.username).then((ulist) => {
-        for (const element of ulist) {
-          if (uuid === element.uuid) {
-            uuid = Math.floor(Math.random() * (max - min + 1)) + min
-          }
-        }
+  const user = {
+    username: body.username,
+    email: body.email,
+    password: body.password
+  }
+  try {
+    const createdUser = await dbUtils.createUser(user)
+    if (!createdUser) {
+      return res.status(500).json({
+        message: 'Something fucked up'
       })
     }
-  })
-
-  // async hashing call
-  bcrypt.hash(body.password, 10, (err, hash) => {
+    return res.status(200).json({
+      message: 'Account created!',
+      username: createdUser.username,
+      email: createdUser.email,
+      uid: createdUser.uid
+    })
+  } catch (err) {
     console.error(err)
-    const newUser = {
-      email: body.email,
-      username: body.username,
-      password: hash,
-      uuid: uuid
-    }
-    dbUtils.createUser(newUser, res)
-  })
+    return res.status(500).json({
+      message: 'Something went wrong, sorry!'
+    })
+  }
 })
-
-/* GET logout route */
-// TODO: Figure out if this is actually needed since we are using only JWT for auth
-router.get('/logout')
 
 module.exports = router
